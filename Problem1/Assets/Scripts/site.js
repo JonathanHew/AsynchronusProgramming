c987e5be69a62a8a673eae1afdfc87779e45a69c
Jonathan Hew <71874041+JonathanHew@users.noreply.github.com>
2022-10-20 12:46:43 +0100

    implemented adding new contact into the table

--- Problem1/Assets/Scripts/site.js
+++ Problem1/Assets/Scripts/site.js
@@ -4,10 +4,11 @@ let contactNumber = document.getElementById("contactNumber");
 let contactEmail = document.getElementById("contactEmail");
 let searchNumber = document.getElementById("searchNumber");
 let msg = document.getElementById("msg");
+let contactTable = document.getElementById("contactTable");
 
-form.addEventListener("submit", (e) => {
+form.addEventListener("button", (e) => {
+  e.preventDefault();
   formValidation();
-  return false;
 });
 
 let formValidation = () => {
@@ -42,8 +43,32 @@ let data = {};
 
 let acceptData = () => {
   data["contactName"] = contactName.value;
-  data["contactNumber"] = contactName.value;
-  data["contactEmail"] = contactName.value;
+  data["contactNumber"] = contactNumber.value;
+  data["contactEmail"] = contactEmail.value;
 
   console.log(data);
+  createContact();
+};
+
+let createContact = () => {
+  // Create an empty <tr> element and add it to the 1st position of the table:
+  const row = contactTable.insertRow(1);
+
+  // Insert new cells (<td> elements) at the 1st and 2nd position of the "new" <tr> element:
+  const cell1 = row.insertCell(0);
+  const cell2 = row.insertCell(1);
+  const cell3 = row.insertCell(2);
+
+  // Add some text to the new cells:
+  cell1.innerHTML = `${data.contactName}`;
+  cell2.innerHTML = `${data.contactNumber}`;
+  cell3.innerHTML = `${data.contactEmail}`;
+
+  resetForm();
+};
+
+let resetForm = () => {
+  contactName.value = "";
+  contactNumber.value = "";
+  contactEmail.value = "";
 };
