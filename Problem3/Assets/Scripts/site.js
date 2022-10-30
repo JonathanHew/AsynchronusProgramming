895752365d6e7295ee223a241076bdb1e4447aff
Jonathan Hew <71874041+JonathanHew@users.noreply.github.com>
2022-10-30 21:56:11 +0000

    removed bootstrap, implemented display and remove repos section

--- Problem3/Assets/Scripts/site.js
+++ Problem3/Assets/Scripts/site.js
@@ -3,32 +3,33 @@ const uname = document.createElement("p");
 const email = document.createElement("p");
 const ulocation = document.createElement("p");
 const gists = document.createElement("p");
-const errMsg = document.getElementById("errMsg")
-
+const errMsg = document.getElementById("errMsg");
+let searchValue = document.getElementById("searchValue");
+const repoArea = document.getElementById("repoArea");
+const repo = document.getElementsByClassName("repo");
 
 let searchValidation = () => {
-  let searchValue = document.getElementById("searchValue");
   hideError();
+  removeData();
+  removeRepos();
 
   fetch(`https://api.github.com/users/${searchValue.value}`)
-  .then((response) => {
-    if (response.ok) {
+    .then((response) => {
+      if (response.ok) {
+        return response.json();
+      } else {
+        showError();
+        throw new Error("NETWORK RESPONSE ERROR");
+      }
+    })
+    .then((data) => {
+      console.log(data);
+      displayUser(data);
+      displayRepos();
       searchValue.value = "";
-      return response.json();
-    } else {
-      showError();
-      throw new Error("NETWORK RESPONSE ERROR");
-    }
-  })
-  .then(data => {
-    removeData();
-    console.log(data);
-    displayUser(data)
-  })
-  .catch((error) => console.error("FETCH ERROR:", error));
-
-}
-
+    })
+    .catch((error) => console.error("FETCH ERROR:", error));
+};
 
 function displayUser(data) {
   const user = data;
@@ -41,7 +42,7 @@ function displayUser(data) {
 
   //setting user image
   const userImage = user.avatar_url;
-  image.src = userImage
+  image.src = userImage;
 
   //setting username
   userName.innerHTML = user.login;
@@ -52,12 +53,9 @@ function displayUser(data) {
   nameDiv.appendChild(uname);
 
   //setting email
-  if(user.email == null)
-  {
+  if (user.email == null) {
     email.innerHTML = "null";
-  }
-  else
-  {
+  } else {
     email.innerHTML = user.email;
   }
   emailDiv.appendChild(email);
@@ -69,7 +67,30 @@ function displayUser(data) {
   //setting gists
   gists.innerHTML = user.public_gists;
   gistsDiv.appendChild(gists);
-  
+}
+
+function displayRepos() {
+  let repos;
+
+  fetch(`https://api.github.com/users/${searchValue.value}/repos`)
+    .then((res) => res.json())
+    .then((data) => {
+      repos = data;
+    })
+    .then(() => {
+      console.log(repos);
+      repos.forEach(repo => {
+        const repoName = repo.name;
+        const repoDesc = repo.description;
+        const newRepo = document.createElement("div");
+        newRepo.className = "repo";
+        newRepo.innerHTML = `
+          <h3>${repoName}</h3>
+          <p>${repoDesc}</p>
+        `;
+        repoList.appendChild(newRepo);
+      });
+    });
 }
 
 function removeData() {
@@ -79,6 +100,10 @@ function removeData() {
   email.innerHTML = "";
   ulocation.innerHTML = "";
   gists.innerHTML = "";
+}
+
+function removeRepos() {
+  repoList.innerHTML = "";
 }
 
 function showError() {
