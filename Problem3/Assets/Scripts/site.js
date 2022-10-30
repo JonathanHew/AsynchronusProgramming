625a880bc2eb150b271d85546952d54536b33d96
Jonathan Hew <71874041+JonathanHew@users.noreply.github.com>
2022-10-30 17:21:36 +0000

    fetch user profile info implemented

--- Problem3/Assets/Scripts/site.js
+++ Problem3/Assets/Scripts/site.js
@@ -8,17 +8,46 @@ fetch("https://api.github.com/users/jonathanhew")
   })
   .then(data => {
     console.log(data);
-    //displayUser(data)
+    displayUser(data)
   })
   .catch((error) => console.error("FETCH ERROR:", error));
 
-  function displayUser(data) {
-    const user = data;
-    const app = document.getElementById("app");
 
-    const userName = user.login;
-    const heading = document.createElement("h1");
-    heading.innerHTML = userName;
-    app.appendChild(heading);
-    
-  }   
+function displayUser(data) {
+  const user = data;
+  const image = document.getElementById("image");
+  const usernameDiv = document.getElementById("usernameDiv");
+  const nameDiv = document.getElementById("nameDiv");
+  const emailDiv = document.getElementById("emailDiv");
+  const locationDiv = document.getElementById("locationDiv");
+  const gistsDiv = document.getElementById("gistsDiv");
+
+  const userImage = user.avatar_url;
+  image.src = userImage
+
+  const userName = document.createElement("p");
+  userName.innerHTML = user.login;
+  usernameDiv.appendChild(userName);
+
+  const name = document.createElement("p");
+  name.innerHTML = user.name;
+  nameDiv.appendChild(name);
+
+  const email = document.createElement("p");
+  email.innerHTML = user.email;
+  emailDiv.appendChild(email);
+
+  const location = document.createElement("p");
+  location.innerHTML = user.location;
+  locationDiv.appendChild(location);
+
+  const gists = document.createElement("p");
+  gists.innerHTML = user.public_gists;
+  gistsDiv.appendChild(gists);
+
+
+  //const heading = document.createElement("h1");
+  //heading.innerHTML = userName;
+  //app.appendChild(heading);
+  
+}   
